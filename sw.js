10c42e1516482bdab5501f921ce6d2c806b39623
samsour <sam.sauer@hotmail.de>
2019-12-20 10:55:51 +0100

    Add manifest and service worker

---
+++ sw.js
@@ -0,0 +1,5 @@
+var cacheName = 'alternating-caps';
+var filesToCache = ['/index.html'];
+
+self.addEventListener('install', e => e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(filesToCache))));
+self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(response => response || fetch(e.request))));
